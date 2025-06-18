cf021ed75b742060c54a8d6c4370fda2fc3afa96
Emma Coye <ecoye@unc.edu>
2025-06-17 22:03:59 -0400

    grabbing weather details from api

--- app/Services/WeatherApiService.php
+++ app/Services/WeatherApiService.php
@@ -5,13 +5,36 @@ use Illuminate\Support\Facades\Http;
 
 class WeatherApiService
 {
-    public function getCurrent(float $lat, float $lon): array
+    public function getCurrentWeather(float $lat, float $lon): array
     {
         $res = Http::get('https://api.open-meteo.com/v1/forecast', [
-            'latitude'        => $lat,
-            'longitude'       => $lon,
-            'current_weather' => true,
+            'latitude'  => $lat,
+            'longitude' => $lon,
+            'current'   => 'temperature_2m,relative_humidity_2m,apparent_temperature,weathercode,wind_speed_10m',
+            'timezone'  => 'auto',
         ]);
-        return $res->json('current_weather');
+
+        $c = $res->json('current');
+
+        return [
+            'temperature' => $c['temperature_2m'],
+            'humidity'    => $c['relative_humidity_2m'],
+            'feels_like'  => $c['apparent_temperature'],
+            'windspeed'   => $c['wind_speed_10m'],
+            'condition'   => $this->conditionCode($c['weathercode']), #use the function
+        ];
     }
+
+    # To get the human readable weather condition
+    private function conditionCode(int $code): string
+{
+    return match (true) {
+        $code === 0                    => 'Clear',
+        in_array($code, [1,2,3], true) => 'Cloudy',
+        in_array($code, [61,63,65], true) => 'Rain',
+        in_array($code, [80,81,82], true) => 'Rain showers',
+        default                       => 'Unknown',
+    };
+}
+
 }
