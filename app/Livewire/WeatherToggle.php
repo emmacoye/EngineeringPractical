f46fddfc7b58c688e2273e152ab443c9e5ef94d2
Emma Coye <ecoye@unc.edu>
2025-06-18 16:04:46 -0400

    Fixed loadWeather() issue to allow for new city searches

--- app/Livewire/WeatherToggle.php
+++ app/Livewire/WeatherToggle.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 namespace App\Livewire;
 
 use Livewire\Component;
@@ -16,17 +16,64 @@ class WeatherToggle extends Component
 
     public function mount(WeatherApiService $svc)
     {
-        $this->svc = $svc;
-        $this->loadWeather();
+        $this->loadWeather($svc);
     }
 
-    public function updatedCity()    { $this->loadWeather(); }
+    public function updatedCity()    { $this->loadWeather($svc); }
     public function updatedUnit()    { }
 
     private function loadWeather()
     {
         try {
             $this->weather = $this->svc->getByCity($this->city);
+            $this->error   = null;
+        } catch (\Exception $e) {
+            $this->weather = [];
+            $this->error   = $e->getMessage();
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.weather-toggle');
+    }
+}*/
+
+
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Services\WeatherApiService;
+
+class WeatherToggle extends Component
+{
+    public $city    = 'New York';
+    public $unit    = 'C';
+    public $weather = [];
+    public $error   = null;
+
+    public function mount()
+    {
+        $this->loadWeather();
+    }
+
+    public function updatedCity()
+    {
+        $this->loadWeather();
+    }
+
+    public function updatedUnit()
+    {
+        // conversion is done in Blade
+    }
+
+    public function loadWeather()
+    {
+        // pull the service on demand
+        $svc = app(WeatherApiService::class);
+
+        try {
+            $this->weather = $svc->getByCity($this->city);
             $this->error   = null;
         } catch (\Exception $e) {
             $this->weather = [];
